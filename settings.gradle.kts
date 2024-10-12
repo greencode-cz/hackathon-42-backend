58ac7cb32642916f6d53dcd475f81675b381d2b6
Sergey Kalistratov <sergey.kalistratov@greencode.cz>
2024-10-12 20:23:46 +0200

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "cz.greencode.hackathon42"
---
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -0,0 +1,14 @@
+package com.example.plugins
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respond(HttpStatusCode.OK, "Hello, Peter!")
+        }
+    }
+}
